50f7748bb1afe756595f82d23f0ede5b5f5a43bf
Jahnavi1203 <87400772+Jahnavi1203@users.noreply.github.com>
2022-01-24 19:26:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -10,14 +10,31 @@ Webcam.attach('#camera');
 
 function takeSnapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+ data_uri + '"/>';
+        document.getElementById("result").innerHTML = '<img id="image_taken" src="'+ data_uri + '"/>';
     });
 }
 
 console.log('ml5 version: ', ml5.version);
 
-Classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2_yjI6Cip/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/2_yjI6Cip/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log("Model is Loded!!");
+}
+
+function check(){
+    img = document.getElementById("image_taken");
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results){
+if (error){
+    console.log(error);
+}
+
+    else{
+     console.log(results);
+     document.getElementById("result_object_name").innerHTML = results[0].label;
+     document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
 }
